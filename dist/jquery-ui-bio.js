92cfea9100da86bffed9518c1169eacb57da6944
Haydn King <hjking734@gmail.com>
2013-01-18 12:27:35 +0000

    Include easeljs

--- dist/jquery-ui-bio.js
+++ dist/jquery-ui-bio.js
@@ -2293,8 +2293,7 @@ $.widget("bio.sequence", $.ui.mouse, {
 
         this._calc_sizes();
         this._init_position();
-        this._make_paper();
-        this._init_markers();
+
         this._trigger('completed');
 
         this._mouseInit();
@@ -2316,60 +2315,6 @@ $.widget("bio.sequence", $.ui.mouse, {
         this.pos = 0;
         this.offset = 0;
         this.bw = Math.floor(this.w / base_width);
-    },
-    _make_paper: function(tile_num){
-        var o = this.options,
-            d = $('<div>')
-                .height(this.h)
-                .width(this.w);
-        this.paper = Raphael(d.get(0), this.w, this.h); 
-        this.el.append(d);
-
-        this.paper.rect(-1, this.h2-sep, this.w+2, 2*sep)
-            .attr({
-                stroke: o.tick_color,
-                fill: o.back_color
-            })
-            .toFront();
-    },
-    _redraw: function() {
-        this._update_markers();
-    },
-    _update_markers: function(){
-        var pos = 10 * Math.ceil(this.pos/10),
-            off = (pos - this.pos) * base_width,
-            i = 0,
-            step = 10 * base_width;
-
-        this.markers.attr({x: off});
-        this.markertext.forEach(function(e){
-            e.attr({x: off+i*step, text: 10*i+pos});
-            i += 1;
-        });
-    },
-    _init_markers: function(){
-        var i,x,y,path,
-            o = this.options;
-
-        this.markertext = this.paper.set();
-
-        y = [this.h2-sep-tick, this.h2+sep+tick];
-        path = '';
-        for(i = 10 * Math.ceil(this.pos/10); i < this.pos+this.bw; i+=10){
-            x = (0.5+i-this.pos) * base_width;
-            path = path + 'M'+x+','+y[0]+'L'+x+','+y[1];
-            this.markertext.push(
-                this.paper.text(x, y[1]+tick_text+1, String(i))
-                    .attr({
-                        color: o.tick_color,
-                        'font-height': tick_text
-                    }));
-        }
-        this.markers = this.paper.path(path)
-                .attr({
-                    stroke: o.tick_color
-                })
-                .toBack();
     },
     _mouseStart: function(ev){
         this.mouse = {x: ev.pageX, y: ev.pageY};
